0a9aeb9a03085b6cc731d29f226a27e5593d36d7
istvan-halasz <haistvan@gmail.com>
2022-10-16 21:56:43 +0300

    Managing Cart & Modal State

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 
 function App() {
+  const [cartIsShown, setIsCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setIsCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setIsCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
